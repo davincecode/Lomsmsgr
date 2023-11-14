8cf7355df94740f07d407c2ca3e1c7011885bc15
Vincent <vincent.ybanez@yorkandchapel.com>
2023-11-13 23:15:09 -0700

    Adds login with encryption

--- src/main/java/com/davincecode/onlime/MainApplication.java
+++ src/main/java/com/davincecode/onlime/MainApplication.java
@@ -2,26 +2,23 @@ package com.davincecode.onlime;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Objects;
-
 public class MainApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        String css = Objects.requireNonNull(this.getClass().getResource("styles.css")).toExternalForm();
-        scene.getStylesheets().add(css);
-        // anotherScene.getStylesheets().add(css);
-        stage.setTitle("OnLime Chatter");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+        String css = getClass().getResource("styles.css").toExternalForm();
+        root.getStylesheets().add(css);
+        primaryStage.setTitle("OnLime Chatter");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
